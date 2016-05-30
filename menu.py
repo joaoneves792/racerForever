1fc4205bfd31131208f6b7c8dbb054e02bb8a6cd
Joao Neves <joaoneves792@gmail.com>
2016-05-30 11:41:24 +0100

    Added menu options for Xbox controller and FF

--- menu.py
+++ menu.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.append('./libs')
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from singletons import Window
+from singletons import Window, Controller
 from racerForever import Game
 
 __play__ = False
@@ -30,6 +32,19 @@ class Menu(Gtk.Window):
         self.fullscreen = Gtk.CheckButton("Fullscreen")
         self.fullscreen.set_active(Window.FULLSCREEN)
         self.box.pack_start(self.fullscreen, False, False, 0)
+
+        controller_box = Gtk.Box(spacing=0, orientation=Gtk.Orientation.VERTICAL, homogeneous=False)
+
+        self.controller = Gtk.CheckButton("Use Xbox360 Controller")
+        self.controller.set_active(Controller.ENABLED)
+        controller_box.pack_start(self.controller, False, False, 0)
+
+        self.ff = Gtk.CheckButton("Force Feedback")
+        self.ff.set_active(Controller.FORCE_FEEDBACK)
+        controller_box.pack_start(self.ff, False, False, 0)
+
+        self.box.pack_start(controller_box, False, False, 0)
+
         play = Gtk.Button(label="Play!")
         self.box.pack_end(play, False, False, 0)
         play.connect("clicked", self.play_game)
@@ -42,6 +57,8 @@ class Menu(Gtk.Window):
         Window.WIDTH = int(self.width.get_text())
         Window.HEIGHT = int(self.height.get_text())
         Window.FULLSCREEN = self.fullscreen.get_active()
+        Controller.ENABLED = self.controller.get_active()
+        Controller.FORCE_FEEDBACK = self.ff.get_active()
         self.hide()
         Gtk.main_quit()
 
